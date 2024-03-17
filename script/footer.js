fa156ea7cea1d168d673435f7348073a8f98440a
Kim Berntson <kimberntson@mac.com>
2024-03-17 22:23:48 +0100

    sticky nav

--- script/footer.js
+++ script/footer.js
@@ -5,7 +5,7 @@ class Footer extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     
-    <div class="footer-flex cream">
+    <div class="cream" id="footer-contact">
     <div class="footer-item">
         <img src="img/icons/1c3ae34c6b.png" height="30px" alt="phone icon">
    
@@ -25,7 +25,7 @@ class Footer extends HTMLElement {
   </div>    
     
       
-    <div class="ct bluegray">
+    <div class="ct bluegray" id="footer-copyright">
       <div>
         <h4> Good Girl Communication AB<h4>
         <h6>Copyright &#169 2023 - Alla rättigheter förbehållna</h6>
